6cacb44c0ab9d59c91aae69d00e0289087b5c9e5
Justin Perez <justin@startrev.com>
2020-03-05 21:03:47 -0800

    Add the routing middleware and koa server

--- server.js
+++ server.js
@@ -16,3 +16,13 @@ const handle = app.getRequestHandler();
 const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
 
 app.prepare().then(() => {});
+  const server = new Koa();
+  server.use(session({ secure: true, sameSite: 'none' }, server));
+  server.keys = [SHOPIFY_API_SECRET_KEY];
+  server.use(async (ctx) => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+    ctx.res.statusCode = 200;
+    return
+  });
+});
