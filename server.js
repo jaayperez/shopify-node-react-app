e5a9ce0050a86804823db38ad1ffb25cc6341bc4
Justin Perez <justin@startrev.com>
2020-03-05 23:17:20 -0800

    Add the createShopifyAuth and verifyRequest middleware

--- server.js
+++ server.js
@@ -15,14 +15,30 @@ const handle = app.getRequestHandler();
 
 const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
 
-app.prepare().then(() => {});
+app.prepare().then(() => {
   const server = new Koa();
   server.use(session({ secure: true, sameSite: 'none' }, server));
   server.keys = [SHOPIFY_API_SECRET_KEY];
+
+  server.use(
+    createShopifyAuth({
+      apiKey: SHOPIFY_API_KEY,
+      secret: SHOPIFY_API_SECRET_KEY,
+      scopes: ['read_products'],
+      afterAuth(ctx) {
+        const { shop, accessToken } = ctx.session;
+
+        ctx.redirect('/');
+      },
+    }),
+  );
+
+  server.use(verifyRequest());
   server.use(async (ctx) => {
     await handle(ctx.req, ctx.res);
     ctx.respond = false;
     ctx.res.statusCode = 200;
     return
   });
+
 });
