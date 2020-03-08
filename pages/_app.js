a1e43cfd8b06f7832f04bf4e81a1156d1d236763
Justin Perez <justin@startrev.com>
2020-03-08 06:01:59 -0700

    Add the Shopify API key, the shopOrigin using js-cookie, and the forceRedirect prop to the AppProvider component

--- pages/_app.js
+++ pages/_app.js
@@ -4,17 +4,19 @@ import { AppProvider } from '@shopify/polaris';
 import { Provider } from '@shopify/app-bridge-react';
 import '@shopify/polaris/styles.css';
 import translations from '@shopify/polaris/locales/en.json';
+import Cookies from 'js-cookie';
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
+    const config = { apiKey: API_KEY, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
     return (
       <React.Fragment>
         <Head>
           <title>Shopify App</title>
           <meta charSet="utf-8" />
         </Head>
-        <Provider>
+        <Provider config={config}>
           <AppProvider i18n={translations}>
             <Component {...pageProps} />
           </AppProvider>
