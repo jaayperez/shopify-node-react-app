63e674186752fc7abc8294b12d7451caa8006408
Justin Perez <justin@startrev.com>
2020-03-08 01:08:02 -0800

    Replace the TextStyle component and txt with the EmptyState component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
-import { Layout, Page, TextStyle } from '@shopify/polaris';
+import { EmptyState, Layout, Page } from '@shopify/polaris';
+
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
 const Index = () => (
   <Page>
     <Layout>
-      <TextStyle variation="positive">
-        Shopify App with Node.js, React, and GraphQL
-      </TextStyle>
+      <EmptyState
+        heading="Discount your products temporarily"
+        action={{
+          content: 'Select products',
+          onAction: () => console.log('clicked'),
+        }}
+        image={img}
+      >
+        <p>Select products to change their price temporarily.</p>
+      </EmptyState>
     </Layout>
   </Page>
 );
