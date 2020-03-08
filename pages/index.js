4c408db1582a03c0f26aab9b808ff059071d5f26
Justin Perez <justin@startrev.com>
2020-03-08 11:53:14 -0700

    Add a class that sets a state for the ResourcePicker

--- pages/index.js
+++ pages/index.js
@@ -3,26 +3,31 @@ import { TitleBar } from '@shopify/app-bridge-react';
 
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
-const Index = () => (
-  <Page>
-    <TitleBar
-      primaryAction={{
-        content: 'Select products',
-      }}
-    />
-    <Layout>
-      <EmptyState
-        heading="Discount your products temporarily"
-        action={{
-          content: 'Select products',
-          onAction: () => console.log('clicked'),
-        }}
-        image={img}
-      >
-        <p>Select products to change their price temporarily.</p>
-      </EmptyState>
-    </Layout>
-  </Page>
-);
+class Index extends React.Component {
+  state = { open: false }
+  render() {
+    return (
+      <Page>
+        <TitleBar
+          primaryAction={{
+            content: 'Select products',
+          }}
+        />
+        <Layout>
+          <EmptyState
+            heading="Discount your products temporarily"
+            action={{
+              content: 'Select products',
+              onAction: () => console.log('clicked'),
+            }}
+            image={img}
+          >
+            <p>Select products to change their price temporarily.</p>
+          </EmptyState>
+        </Layout>
+      </Page>
+    );
+  }
+}
 
 export default Index;
