2c3c7e9fc47c6ecb489d42aee39de9f990f00130
Justin Perez <justin@startrev.com>
2020-03-07 16:43:29 -0800

    Import the Polaris CSS styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import App from 'next/app';
 import Head from 'next/head';
 import { AppProvider } from '@shopify/polaris';
+import '@shopify/polaris/styles.css';
 
 class MyApp extends App {
   render() {
