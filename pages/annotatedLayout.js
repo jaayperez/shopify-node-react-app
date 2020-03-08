bc38f91e6b44e6980a28cc65e64bd28b43d561c8
Justin Perez <justin@startrev.com>
2020-03-08 05:15:27 -0700

    Replace the content and destructure the state for ease of use

--- pages/annotatedLayout.js
+++ pages/annotatedLayout.js
@@ -1,24 +1,62 @@
-import { Card, Layout, Page } from '@shopify/polaris';
+import {
+  Button,
+  Card,
+  Form,
+  FormLayout,
+  Layout,
+  Page,
+  Stack,
+  TextField,
+} from '@shopify/polaris';
 
 class AnnotatedLayout extends React.Component {
-  state = {};
+  state = {
+    discount: '10%',
+  };
 
   render() {
+    const { discount } = this.state;
+
     return (
       <Page>
         <Layout>
           <Layout.AnnotatedSection
             title="Default discount"
-            description="Add a product to Sample App, it will automatically be discounted."
+            description="Add a product to Shopify App, it will automatically be discounted."
           >
             <Card sectioned>
-              <div>Card</div>
+              <Form onSubmit={this.handleSubmit}>
+                <FormLayout>
+                  <TextField
+                    value={discount}
+                    onChange={this.handleChange('discount')}
+                    label="Discount percentage"
+                    type="discount"
+                  />
+                  <Stack distribution="trailing">
+                    <Button primary submit>
+                      Save
+                    </Button>
+                  </Stack>
+                </FormLayout>
+              </Form>
             </Card>
           </Layout.AnnotatedSection>
         </Layout>
       </Page>
     );
   }
+
+  handleSubmit = () => {
+    this.setState({
+      discount: this.state.discount,
+    });
+    console.log('submission', this.state);
+  };
+
+  handleChange = (field) => {
+    return (value) => this.setState({ [field]: value });
+  };
 }
 
 export default AnnotatedLayout;
