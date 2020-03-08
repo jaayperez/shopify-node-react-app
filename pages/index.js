e329dbf1cb13cd2f0036482267044f0a0c914430
Justin Perez <justin@startrev.com>
2020-03-08 11:51:59 -0700

    Add the Title Bar from App Bridge React

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import { EmptyState, Layout, Page } from '@shopify/polaris';
+import { TitleBar } from '@shopify/app-bridge-react';
 
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
 const Index = () => (
   <Page>
+    <TitleBar
+      primaryAction={{
+        content: 'Select products',
+      }}
+    />
     <Layout>
       <EmptyState
         heading="Discount your products temporarily"
