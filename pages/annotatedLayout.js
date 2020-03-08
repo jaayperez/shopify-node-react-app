e86194432a91fbfdeaaf4cd98c340de045763fbb
Justin Perez <justin@startrev.com>
2020-03-08 05:33:38 -0700

    Import the SettingToggle component and add a handler to toggle the state

--- pages/annotatedLayout.js
+++ pages/annotatedLayout.js
@@ -5,16 +5,22 @@ import {
   FormLayout,
   Layout,
   Page,
+  SettingToggle,
   Stack,
   TextField,
+  TextStyle,
 } from '@shopify/polaris';
 
 class AnnotatedLayout extends React.Component {
   state = {
     discount: '10%',
+    enabled: false,
   };
 
   render() {
+    const { discount, enabled } = this.state;
+    const contentStatus = enabled ? 'Disable' : 'Enable';
+    const textStatus = enabled ? 'enabled' : 'disabled';
     const { discount } = this.state;
 
     return (
@@ -42,6 +48,21 @@ class AnnotatedLayout extends React.Component {
               </Form>
             </Card>
           </Layout.AnnotatedSection>
+          <Layout.AnnotatedSection
+            title="Price updates"
+            description="Temporarily disable all Sample App price updates"
+          >
+            <SettingToggle
+              action={{
+                content: contentStatus,
+                onAction: this.handleToggle,
+              }}
+              enabled={enabled}
+            >
+              This setting is{' '}
+              <TextStyle variation="strong">{textStatus}</TextStyle>.
+            </SettingToggle>
+          </Layout.AnnotatedSection>
         </Layout>
       </Page>
     );
@@ -53,9 +74,13 @@ class AnnotatedLayout extends React.Component {
     });
     console.log('submission', this.state);
   };
-
   handleChange = (field) => {
     return (value) => this.setState({ [field]: value });
+  };
+  handleToggle = () => {
+    this.setState(({ enabled }) => {
+      return { enabled: !enabled };
+    });
   };
 }
 
