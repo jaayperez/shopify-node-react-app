0ede0715ae69d26ca1504cff9e369f9d69de3bc0
Justin Perez <justin@startrev.com>
2020-03-10 01:53:43 -0700

    Add a constant that includes the query

---
+++ components/ResourceList.js
@@ -0,0 +1,28 @@
+const GET_PRODUCTS_BY_ID = gql`
+  query getProducts($ids: [ID!]!) {
+    nodes(ids: $ids) {
+      ... on Product {
+        title
+        handle
+        descriptionHtml
+        id
+        images(first: 1) {
+          edges {
+            node {
+              originalSrc
+              altText
+            }
+          }
+        }
+        variants(first: 1) {
+          edges {
+            node {
+              price
+              id
+            }
+          }
+        }
+      }
+    }
+  }
+`;
