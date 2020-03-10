0ce16ef42362d5311fce97ac84cfa729aa20ff38
Justin Perez <justin@startrev.com>
2020-03-10 00:30:41 -0700

    Add the write_products to the shopifyAuth function

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.prepare().then(() => {
     createShopifyAuth({
       apiKey: SHOPIFY_API_KEY,
       secret: SHOPIFY_API_SECRET_KEY,
-      scopes: ['read_products'],
+      scopes: ['read_products', 'write_products'],
       afterAuth(ctx) {
         const { shop, accessToken } = ctx.session;
         ctx.cookies.set('shopOrigin', shop, {
