e9302d167059c7a2e90a559414d434a3fac63fa6
Justin Perez <justin@startrev.com>
2020-03-10 00:32:32 -0700

    Restart the server

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5d8bf28dcafde7deca71";
+/******/ 	var hotCurrentHash = "475dde9eeb5c7793f40f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
