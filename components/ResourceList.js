e8230030ec266fe236569e572f267a439a9368a5
Justin Perez <justin@startrev.com>
2020-03-10 22:47:07 -0700

    Add the query variables and store-js import

--- components/ResourceList.js
+++ components/ResourceList.js
@@ -1,6 +1,7 @@
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import { Card } from '@shopify/polaris';
+import store from 'store-js';
 
 const GET_PRODUCTS_BY_ID = gql`
   query getProducts($ids: [ID!]!) {
@@ -34,7 +35,7 @@ const GET_PRODUCTS_BY_ID = gql`
 class ResourceListWithProducts extends React.Component {
   render() {
     return (
-      <Query query={GET_PRODUCTS_BY_ID}>
+      <Query query={GET_PRODUCTS_BY_ID} variables={{ ids: store.get('ids') }}>
         {({ data, loading, error }) => {
           if (loading) return <div>Loading…</div>;
           if (error) return <div>{error.message}</div>;
