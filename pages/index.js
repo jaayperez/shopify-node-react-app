6818939f6fdfc55a7b506ef6afdcbf43a7d62662
Justin Perez <justin@startrev.com>
2020-03-10 22:50:19 -0700

    Import and add the ResourceListWithProducts component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { EmptyState, Layout, Page } from '@shopify/polaris';
 import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
 import store from 'store-js';
+import ResourceListWithProducts from '../components/ResourceList';
 
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
@@ -33,6 +34,7 @@ class Index extends React.Component {
             <p>Select products to change their price temporarily.</p>
           </EmptyState>
         </Layout>
+        <ResourceListWithProducts />
       </Page>
     );
   }
