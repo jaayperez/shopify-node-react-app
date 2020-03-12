a8f02885c7ae86f671f900f89fc9215d8548b35c
Justin Perez <justin@startrev.com>
2020-03-11 22:10:22 -0700

    Add the Redirect and Context imports

--- components/ResourceList.js
+++ components/ResourceList.js
@@ -8,6 +8,8 @@ import {
   Thumbnail,
 } from '@shopify/polaris';
 import store from 'store-js';
+import { Redirect } from '@shopify/app-bridge/actions';
+import { Context } from '@shopify/app-bridge-react';
 
 const GET_PRODUCTS_BY_ID = gql`
   query getProducts($ids: [ID!]!) {
@@ -39,8 +41,21 @@ const GET_PRODUCTS_BY_ID = gql`
 `;
 
 class ResourceListWithProducts extends React.Component {
+
+  static contextType = Context;
+
   render() {
+    const app = this.context;
+    const redirectToProduct = () => {
+      const redirect = Redirect.create(app);
+      redirect.dispatch(
+        Redirect.Action.APP,
+        '/edit-products',
+      );
+    };
+
     const twoWeeksFromNow = new Date(Date.now() + 12096e5).toDateString();
+
     return (
       <Query query={GET_PRODUCTS_BY_ID} variables={{ ids: store.get('ids') }}>
         {({ data, loading, error }) => {
@@ -74,6 +89,10 @@ class ResourceListWithProducts extends React.Component {
                       id={item.id}
                       media={media}
                       accessibilityLabel={`View details for ${item.title}`}
+                      onClick={() => {
+                        store.set('item', item);
+                        redirectToProduct();
+                      }}
                     >
                       <Stack>
                         <Stack.Item fill>
